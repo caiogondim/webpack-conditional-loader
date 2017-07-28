30e482b075029f4ceb14ce24976bf918ef0a5bcd
Caio Gondim <me@caiogondim.com>
2017-07-28 10:26:32 -0400

    Move test to tap

--- test/index.js
+++ test/index.js
@@ -1,25 +1,25 @@
-const test = require('tape')
+const tap = require('tap')
 const truthy = require('../build/truthy')
 const falsey = require('../build/falsey')
 const envVarTruthy = require('../build/env-var-truthy')
 const envVarFalsey = require('../build/env-var-falsey')
 
-test('comment blocks with falsey predicate', (test) => {
+tap.test('comment blocks with falsey predicate', (test) => {
   test.equal(falsey, 1)
   test.end()
 })
 
-test('dont comment blocks with truthy predicate', (test) => {
+tap.test('dont comment blocks with truthy predicate', (test) => {
   test.equal(truthy, 2)
   test.end()
 })
 
-test('dont comment env var blocks with truthy predicate', (test) => {
+tap.test('dont comment env var blocks with truthy predicate', (test) => {
   test.equal(envVarTruthy, 2)
   test.end()
 })
 
-test('comment env var blocks with falsey predicate', (test) => {
+tap.test('comment env var blocks with falsey predicate', (test) => {
   test.equal(envVarFalsey, 1)
   test.end()
 })

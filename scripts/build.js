dd5101be4d7f3a9bda3526e8a1a808478bb1fd24
Caio Gondim <me@caiogondim.com>
2017-07-27 23:36:28 -0400

    Simplify package.json

--- scripts/build.js
+++
@@ -1,9 +0,0 @@
-const util = require('util')
-const webpack = util.promisify(require('webpack'))
-const webpackConfig = require('../webpack.js')
-
-function build() {
-  return webpack(webpackConfig)
-}
-
-build().catch((error) => console.error(error))
